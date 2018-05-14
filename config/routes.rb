ba51e5729b5b003822b48a5f63873ce7c581d4db
mixplugs <mix.plugs@gmail.com>
2018-05-15 01:49:08 +0800

    add main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :servers
+  resources :players
+  resources :items
+  resources :creatures
+  resources :quests
+
+  root 'main#index'
 end
