ae0e042fa4d4836907b11d5eaa9c3090cf4fdbee
mixplugs <mix.plugs@gmail.com>
2018-05-14 23:37:30 +0800

    add models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_many :holds
+  has_many :players through: :holds
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
   belongs_to :server
+  has_many :holds
+  has_many :items, through: :holds
 end
