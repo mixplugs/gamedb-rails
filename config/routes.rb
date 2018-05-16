642d6201efb8c45fceb2f82157a6d5497ac6e841
mixplugs <mix.plugs@gmail.com>
2018-05-16 20:18:28 +0800

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :servers
-  # resources :players
-  # resources :items
-  # resources :creatures
-  # resources :quests
+  resources :servers do
+    resources :players
+  end
+  
+  resources :items
+  resources :creatures
+  resources :quests
 
   root 'main#index'
 end
