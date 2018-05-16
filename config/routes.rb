cfdc246fca31a9d0bde833cfb4713db2c6d040c9
mixplugs <mix.plugs@gmail.com>
2018-05-16 18:52:51 +0800

    removed reses

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :servers
-  resources :players
-  resources :items
-  resources :creatures
-  resources :quests
+  # resources :servers
+  # resources :players
+  # resources :items
+  # resources :creatures
+  # resources :quests
 
   root 'main#index'
 end
