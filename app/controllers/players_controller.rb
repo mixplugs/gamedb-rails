03db06b19018a874afeb09ac4acb7de962819896
mixplugs <mix.plugs@gmail.com>
2018-05-16 23:47:01 +0800

    add player list

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,12 @@
 class PlayersController < ApplicationController
+  def create
+		@server = server.find(params[:server_id])
+		@player = @server.players.create(player_params)
+		redirect_to server_path(@server)
+	end
+
+	private
+		def player_params
+			params.require(:player).permit(:name, :level, :health, :career)
+		end
 end
