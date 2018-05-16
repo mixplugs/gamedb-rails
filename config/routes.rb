0fa99c64c3b1992e053420a2704c13b7e3d5ea86
mixplugs <mix.plugs@gmail.com>
2018-05-16 19:04:10 +0800

    tried to use bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :servers
+  resources :servers
   # resources :players
   # resources :items
   # resources :creatures
